162e9ab0d82d40589c75a21c53a0eca8054f1e4e
vatsalgabani10 <vatsalgabani10@gmail.com>
2024-03-12 17:09:46 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiErr, ApiError } from "../utils/ApiError.js"
-
+import { ApiErr, ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse    } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     //get user details from frontend
@@ -31,32 +33,55 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
 
-    let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) { 
-        coverImageLocalPath= req.files.coverImage[0].path;  
-    }
+    // let coverImageLocalPath;
+    // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) { 
+    //     coverImageLocalPath= req.files.coverImage[0].path;  
+    // }
+    
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;  
 
     if (!avatarLocalPath) { 
         throw new ApiError(400, "Avatar file is required")
     }
 
-    const user = await User.create({
+    uploadOnCloudinary(avatarLocalPath )
+
+    // const user = await User.create({
+    //     fullName,
+    //     avatar: avatar.url,
+    //     coverImage: coverImage?.url || "",
+    //     email,
+    //     password,
+    //     username: username.toLowerCase()
+    // })
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+    
+    const user= await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
         password,
-        username: username.toLowerCase()
+        username:username.toLowerCase()
     })
 
-    const createUser = await User.find
+
+    const createdUser = User.findById(user._id).select("-password -refreshToken  ")
 
 
-    
+    if (!createdUser) {
+        throw new ApiError(500, "Server error while creating the account")
 
+    }
 
-
-
+    return res.Status(201).json(
+    new ApiResponse(200, createdUser, "User registered Successfully ")
+)
 
 
 
